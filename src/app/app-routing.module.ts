f6736532a45d5831626e54781b773a8cb7f524df
Ramanath Madiwal <ramanathrm.kumta@gmail.com>
2019-03-20 16:35:13 +0530

    Fully workings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,9 @@ import { ChannelSessionsComponent} from './components/channelsessions/channelses
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 
-
 const routes: Routes = [
   {path:'',component:HomeComponent,pathMatch: 'full'},
   {path:'channel',component:ChannelSessionsComponent},
-  {path:'',component:HomeComponent,pathMatch: 'full'},
   { path: 'not-found', component: PageNotFoundComponent, data: {message: 'Page not found!'} },
   { path: '**', redirectTo: '/not-found' }
 
